8aa0332babe3793ccbfa2e32fb3dceeed517f58f
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2021-11-24 13:09:41 -0500

    remove subtrees

--- registry-api-service/src/main/java/gov/nasa/pds/api/engineering/exceptions/ApplicationTypeException.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.pds.api.engineering.exceptions;
-
-public class ApplicationTypeException extends Exception
-{
-	private static final long serialVersionUID = 4336719956245071997L;
-
-	public ApplicationTypeException(String message)
-	{ super(message); }
-}
