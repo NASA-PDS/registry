f1ebeea2eca48947734eac42f1c1fb84ae0d14ed
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-01-07 09:26:07 -0800

    Fix linter issues

--- src/pds/registry/utils/geostac/create_lola_pds4.py
+++ src/pds/registry/utils/geostac/create_lola_pds4.py
@@ -2,16 +2,17 @@
 import argparse
 import importlib
 import logging
-import os
 from datetime import date
 from pathlib import Path
 
 import requests
-from jinja2 import Environment, select_autoescape
+from jinja2 import Environment
+from jinja2 import select_autoescape
 from pds.registry.utils.geostac import templates
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def check_for_overlap(bbox1, bbox2):
     """Checks if bounding boxes overlap anywhere.
@@ -93,7 +94,7 @@ def create_product_external(item):
         item_title = item["assets"]["data"]["title"]
 
         last_slash_i = item["assets"]["data"]["href"].rfind("/")
-        file = "data/" +item["assets"]["data"]["href"][last_slash_i + 1:]
+        file = "data/" + item["assets"]["data"]["href"][last_slash_i + 1:]
         logger.info(f'file is on {item["assets"]["data"]["href"]},fake file is on {file}')
         open("lola_xml/product_external/" + file, 'a').close()
 
