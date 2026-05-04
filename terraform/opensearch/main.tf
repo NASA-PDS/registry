8acb2e3941839ace2c5d3c27c113c0077e62a06b
jimmie <jimmie.d.young@gmail.com>
2026-05-04 16:11:57 -0700

    Correct committed versions of main and variable tf files.

--- terraform/opensearch/main.tf
+++ terraform/opensearch/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "pds-prod-infra"
+    key    = "provisioned_opensearch/provisioned_opensearch.tfstate"
+    region = "us-west-2"
+  }
+}
+
 data "aws_caller_identity" "current" {}
 
 # Load external JSON files
@@ -11,12 +19,12 @@ resource "aws_opensearch_domain" "pds-opensearch-domain" {
 
   cluster_config {
     instance_type  = "${var.data_node_instance_type}"
-    instance_count = ${var.data_node_count} 
+    instance_count = var.data_node_count
 
     # 3 x m6g.large.search dedicated master nodes
     dedicated_master_enabled = true
     dedicated_master_type    = "${var.master_node_instance_type}"
-    dedicated_master_count   = ${var.master_node_count}
+    dedicated_master_count   = var.master_node_count
 
     # Multi-AZ is required for 3 dedicated master nodes to ensure HA
     zone_awareness_enabled = true
@@ -27,17 +35,17 @@ resource "aws_opensearch_domain" "pds-opensearch-domain" {
 
   ebs_options {
     ebs_enabled = true
-    volume_type = "gp3"
+    volume_type = "${var.ebs_volume_type}"
     # EBS volume size is specified in GB per data node
-    volume_size = $var.ebs_volume_gb
+    volume_size = var.ebs_volume_gb
   }
 
   encrypt_at_rest {
-    enabled = true
+    enabled = var.encryption_at_rest
   }
 
   node_to_node_encryption {
-    enabled = true
+    enabled = var.n2n_encryption
   }
 
   domain_endpoint_options {
@@ -47,36 +55,36 @@ resource "aws_opensearch_domain" "pds-opensearch-domain" {
 }
 
 data "aws_iam_policy_document" "domain_access_policy_document" {
-  description = "Data access policies to define access to ${var.domain_name}"
+  dynamic "statement" {
+    for_each = local.access_policies
 
-  policy = jsonencode({
-    "Version"   = "2012-10-17",
-    "Statement" = [
-      for statement in local.access_policies.Statememts : {
-        Sid         = statement.Sid,
-        Effect      = statement.Effect,
-        Action      = statement.Action,
-        Resource    = [
-          for resource in statement.Resource : 
+    content {
+      sid         = statement.value.Sid
+      effect      = statement.value.Effect
+      actions     = statement.value.Action
+      resources   = [
+        for resource in statement.value.Resource : 
+          replace(
             replace(
               replace(
-                replace(
-                  resource, 
-                  "{account_id}". data.aws_caller_identity.current.account_id
-                ),
-                "{region}", "${var.region}"
+                resource, 
+                "{account_id}", data.aws_caller_identity.current.account_id
               ),
-              "{domain_name}", "${var.domain_name}"
-            )
-        ],
-        Principal   = [for principal in rule.Principal : replace(principal, "{account_id}", data.aws_caller_identity.current.account_id)]
+              "{region}", "${var.aws_region}"
+            ),
+            "{domain_name}", "${var.domain_name}"
+          )
+      ]
+      principals {
+          type        = "AWS"
+          identifiers = [ for principal in statement.value.Principal : replace(principal, "{account_id}", data.aws_caller_identity.current.account_id) ]
       }
-    ]
-  })
+    }
+  }
 }
 
 resource "aws_opensearch_domain_policy" "domain_access_policy" {
   domain_name = "${var.domain_name}"
-  access_policies = data.aws_iam_policy_document.domain_access_policy_document
+  access_policies = data.aws_iam_policy_document.domain_access_policy_document.json
 }
 
