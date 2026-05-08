d23b2e25ca1adf7d285fcf15e5a7fc4ab8518217
jimmie <jimmie.d.young@gmail.com>
2026-05-08 13:39:52 -0700

    Initial commit of index template tf scripts.

---
+++ terraform/opensearch/index_templates/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket = "pds-prod-infra"
+    key    = "provisioned_opensearch/opensearch_index_templates.tfstate"
+    region = "us-west-2"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+# Load external JSON files
+locals {
+  index_templates = jsondecode(file("${var.template_file_pathname}"))
+}
+
+resource "opensearch_composable_index_template" "pds_index_templates" {
+  for_each = local.index_templates
+
+  name = each.key
+  body = jsonencode({
+    index_patterns = each.value.index_pattern
+    priority       = each.value.priority
+    template = {
+      settings = {
+          "index.number_of_shards"   = each.value.shards
+          "index.number_of_replicas" = each.value.replicas
+      }
+    }
+  })
+}
+
