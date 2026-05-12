164dd64d0a3ee4aed8d169b9260470fa32aa2bb7
jimmie <jimmie.d.young@gmail.com>
2026-05-12 12:41:47 -0700

    Revise pipeline terraform scripts.

--- terraform/opensearch/osi_pipeline/main.tf
+++ terraform/opensearch/osi_pipeline/main.tf
@@ -7,15 +7,23 @@ terraform {
 }
 
 resource "aws_osis_pipeline" "pds_osi_pipeline" {
-  pipeline_name              = "${var.pipeline_name}"
+  pipeline_name               = "${var.pipeline_name}"
   pipeline_configuration_body = file("${var.pipeline_config_yaml_file}")
-      max_units                  = var.pipeline_max_units
-      min_units                  = var.pipeline_min_units 
+  pipeline_role_arn           = "{var.pipeline_role_arn}"
+
+  max_units                   = var.pipeline_max_units
+  min_units                   = var.pipeline_min_units 
 
   log_publishing_options {
     is_logging_enabled = true
     cloudwatch_log_destination {
-      log_group = "/aws/vendedlogs/OpenSearchService/ingestion/${var.pipeline_name}"
+      log_group = "${var.cloudwatch_log_group}"
     }
+  }
+
+  vpc_options {
+    subnet_ids              = [ "${var.subnet_ids}" ]
+    security_group_ids      = [ "${var.security_group_ids}" ]
+    vpc_endpoint_management = "${var.vpc_endpoint_management}"
   }
 }
