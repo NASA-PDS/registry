f023c6c0d052f9c3f858632d50a8514df8a1188b
jimmie <jimmie.d.young@gmail.com>
2026-05-12 12:41:47 -0700

    Revise pipeline terraform scripts.

--- terraform/opensearch/osi_pipeline/variables.tf
+++ terraform/opensearch/osi_pipeline/variables.tf
@@ -13,6 +13,11 @@ variable "pipeline_config_yaml_file" {
   description = "Full path to the pipeline config yaml file."
 }
 
+variable "pipeline_role_arn" {
+  type        = string
+  description = "ARN of the pipeline role."
+}
+
 variable "pipeline_max_units" {
   type        = number
   description = "Maximum number of pipeline ingestion units."
@@ -22,6 +27,26 @@ variable "pipeline_min_units" {
   type        = number
   description = "Minimum number of pipeline ingestion units. Default = 1."
   default     = 1
+}
+
+variable "cloudwatch_log_group" {
+  type        = string
+  description = "Log group for pipeline logging."
+}
+
+variable "subnet_ids" {
+  type        = string
+  description = "Subnet through which to access the pipeline."
+}
+
+variable "security_group_ids" {
+  type        = string
+  description = "Security group associated with the endpoint."
+}
+
+variable "vpc_endpoint_management" {
+  type        = string
+  description = "CUSTOMER or SERVICE - who is managing the vpc endpoint."
 }
 
 variable "venue" {
