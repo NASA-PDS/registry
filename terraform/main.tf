2284340ea1eddfcaf8a613ed6a10b054b0103e10
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2026-07-15 14:10:17 -0700

    replace commented obsolete content by a TODO comment

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,22 +56,7 @@ module "api_gateway" {
   common_tags          = var.common_tags
 }
 
-# TL: not ready for integration as we have a deadline for the ISRO node creation in dev, step done manuallyt
-# module "registry_api" {
-#   source = "git::https://github.com/NASA-PDS/registry-api.git//terraform?ref=terraform_for_dev"
-#
-#   aws_region = var.aws_region
-#   sprint_boot_args = "--openSearch.host=${module.opensearch.collection_endpoint} --openSearch.CCSEnabled=true --openSearch.username='' --openSearch.disciplineNodes=${join(",", var.node_list)} --registry.service.version=1.6.0-SNAPSHOT"
-#   aws_s3_bucket_logs_id=var.aws_s3_bucket_logs_id
-#   registry_api_docker_image=var.registry_api_docker_image
-#   ecs_task_role = data.aws_ssm_parameter.registry_api_ecs_task_role_arn
-#   ecs_task_execution_role = data.aws_ssm_parameter.registry_api_ecs_task_execution_role_arn
-#   aws_fg_vpc = var.vpc_id
-#   aws_fg_security_group = var.registry_api_ecs_service_security_group
-#   aws_fg_subnets = var.subnet_ids
-#   aws_lb_subnets = var.public_subnet_ids
-#   aws_acm_certificate_arn = var.acm_certificate_arn
-#
-#   common_tags = var.common_tags
-#
-# }
+# TODO add modules call for registry-api and registry-sweepers
+# make the opensearch configuration pulled from the SSM parameters in the module opensearch_serverless
+# or opensearch_managed as the module opensearch might not be called, especially in a prod configuration
+# where we want to keep the same database
