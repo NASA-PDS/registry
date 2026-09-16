9bd98e5202ee8d17162337fd4b8747cb53fcc1d5
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2026-09-15 21:03:43 -0700

    integrate sweepers as a sub-module of the terraform

--- terraform/applications/outputs.tf
+++ terraform/applications/outputs.tf
@@ -1,52 +1,64 @@
 # Root Outputs
-# These outputs expose values from the sub-modules
+# These outputs expose values from the credentials_api module
 
-
-
-# Lambda Outputs
 output "lambda_function_name" {
   description = "Name of the Lambda function"
-  value       = module.lambda.lambda_function_name
+  value       = module.credentials_api.lambda_function_name
 }
 
 output "lambda_function_arn" {
   description = "ARN of the Lambda function"
-  value       = module.lambda.lambda_function_arn
+  value       = module.credentials_api.lambda_function_arn
 }
 
 output "lambda_log_group_name" {
   description = "Name of the Lambda CloudWatch Log Group"
-  value       = module.lambda.lambda_log_group_name
+  value       = module.credentials_api.lambda_log_group_name
 }
 
 output "lambda_log_group_arn" {
   description = "ARN of the Lambda CloudWatch Log Group"
-  value       = module.lambda.lambda_log_group_arn
+  value       = module.credentials_api.lambda_log_group_arn
 }
 
 output "cognito_jwks_url" {
   description = "Cognito JWKS URL used for JWT token validation"
-  value       = module.lambda.cognito_jwks_url
+  value       = module.credentials_api.cognito_jwks_url
 }
 
-
-# API Gateway Outputs
 output "api_gateway_id" {
   description = "ID of the API Gateway"
-  value       = module.api_gateway.api_id
+  value       = module.credentials_api.api_gateway_id
 }
 
 output "api_gateway_endpoint" {
   description = "Base URL of the API Gateway"
-  value       = module.api_gateway.api_endpoint
+  value       = module.credentials_api.api_gateway_endpoint
 }
 
 output "credentials_endpoint" {
   description = "Full URL for the GET /credentials endpoint"
-  value       = module.api_gateway.credentials_endpoint
+  value       = module.credentials_api.credentials_endpoint
 }
 
 output "node_list" {
   description = "List of discipline nodes"
-  value       = var.node_list
+  value       = module.credentials_api.node_list
+}
+
+# registry-sweepers outputs
+
+output "sweepers_task_definition_arns" {
+  description = "Map of node name to ECS task definition ARN"
+  value       = module.registry_sweepers.task_definition_arns
+}
+
+output "sweepers_log_group_names" {
+  description = "Map of node name to CloudWatch log group name"
+  value       = module.registry_sweepers.log_group_names
+}
+
+output "sweepers_task_role_arn" {
+  description = "Sweeper task role ARN needed to update the OpenSearch data access policy"
+  value       = module.registry_sweepers.sweeper_task_role
 }
